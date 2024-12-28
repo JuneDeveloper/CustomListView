3239aa2fc501348274be5faf1e5d06d194f05938
JuneDeveloper <ivan.iw.ko2017@gmail.com>
2024-12-28 18:53:37 +0800

    Добавлены пункты согласно заданию CustomListView 2.

---
+++ app/src/main/java/com/example/customlistview/DetailsActivity.kt
@@ -0,0 +1,36 @@
+package com.example.customlistview
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var toolbarTB:androidx.appcompat.widget.Toolbar
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+        toolbarTB = findViewById(R.id.toolbarTB)
+        setSupportActionBar(toolbarTB)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.exitItem -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
--- app/src/main/java/com/example/customlistview/MainActivity2.kt
+++ app/src/main/java/com/example/customlistview/MainActivity2.kt
@@ -1,15 +1,10 @@
 package com.example.customlistview
 
 import android.annotation.SuppressLint
-import android.app.Person
-import android.content.ClipData.Item
 import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.AdapterView
@@ -17,12 +12,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ListView
-import android.widget.Toolbar
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import java.io.IOException
 
 class MainActivity2 : AppCompatActivity(),Removable,Updatable {
